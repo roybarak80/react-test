db46acc844a57370a72d3b1f699d511212a07728
roybarak80 <barakroy@gmail.com>
2018-10-12 02:24:11 +0300

    Updates

---
+++ precache-manifest.c7d503cc8e1968478e10df2e1edcce3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "745cc6a50a47403619a0",
+    "url": "/react-test/static/js/runtime~main.745cc6a5.js"
+  },
+  {
+    "revision": "9da13ad7e70335602e7d",
+    "url": "/react-test/static/js/main.9da13ad7.chunk.js"
+  },
+  {
+    "revision": "5bd39161dfeb1bd0e015",
+    "url": "/react-test/static/js/1.5bd39161.chunk.js"
+  },
+  {
+    "revision": "9da13ad7e70335602e7d",
+    "url": "/react-test/static/css/main.7778cbdd.chunk.css"
+  },
+  {
+    "revision": "33391d1fccabd3cc5e5c30d40d559bf0",
+    "url": "/react-test/index.html"
+  }
+];
